265eae352b88ddf0b957ce69f56609dc6cf90695
Bell App Lab <apps@bellapplab.com>
2016-10-06 16:53:26 +0100

    Enough

--- Example/Pods/Target Support Files/Internet/Internet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Internet : NSObject
-@end
-@implementation PodsDummy_Internet
-@end
--- Example/Pods/Target Support Files/Pods-Internet_Example/Pods-Internet_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Internet_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Internet_Example
-@end
